2e7ab45c203a6a2964dc79787d54801982abbe96
Benou <prado.benjamin@hotmail.com>
2021-03-30 14:44:11 +0200

    Fonction 1

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
+ *
  */
 class User implements UserInterface
 {
